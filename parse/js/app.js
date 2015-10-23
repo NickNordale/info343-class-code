03eff373f9e26f256b7c8a646f1e86d0b6e52c0d
Nick-Nordale <nicknordale@gmail.com>
2015-10-22 17:41:44 -0700

    noise

--- parse/js/app.js
+++ parse/js/app.js
@@ -53,14 +53,19 @@ $(function() {
     function renderTasks() {
         tasksList.empty();
         tasks.forEach(function(task) {
-            $(document.createElement('li'))
-                .text(task.get('title') + ' : ' + task.get('rating'))
+            var li = $(document.createElement('li'))
+                .text(task.get('title'))
                 .addClass(task.get('done') ? 'completed-task' : '')
                 .appendTo(tasksList)
                 .click(function() {
                     task.set('done', !task.get('done'));
                     task.save().then(renderTasks, displayError);
                 });
+
+            $(document.createElement('span'))
+                .raty({readOnly: true, score: (task.get('rating') || 0),
+                                        hints: ['awful', 'bad', 'decent', 'good', 'awesome']})
+                .appendTo(li);
         });
     }
 
