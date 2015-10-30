d8a3c57e1cf269e4e1cb8d0b42767822b46bce03
Nick-Nordale <nicknordale@gmail.com>
2015-10-29 21:36:02 -0700

    Class code

--- angular/js/movies.js
+++ angular/js/movies.js
@@ -1,10 +1,33 @@
 
 angular.module('Movies', [])
     .controller('MoviesC', function($scope, $http) {
+        var ratingsMap = {
+            'Not Rated': 0,
+            'G': 1,
+            'PG': 2,
+            'PG-13': 3,
+            'R': 4,
+            'NC-17': 5,
+            'X': 6
+        };
         $http.get('data/movies-2014.min.json')
             .then(function(results) {
                 // new array on scope called movies
                 // this is the array of movies from the JSON file
-                $scope.movies = results.data;
+                $scope.movies = results.data.map(function(movie) {
+                    movie.ratingOrdinal = ratingsMap[movie.rating];
+                    return movie;
+                });
+
+                $scope.distributors = _.uniq(_.pluck($scope.movies, 'distributor'));
             });
+
+        $scope.setSort = function(propName) {
+            if ($scope.sortCol === propName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            } else {
+                $scope.sortCol = propName;
+                $scope.sortReverse = false;
+            }
+        }
     });
